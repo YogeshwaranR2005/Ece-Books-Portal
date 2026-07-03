c9454e8ee789ce3ab14848c1260f8254d3839323
Yogeshwaran <yogeshrami2005@gmail.com>
2026-07-04 01:18:28 +0530

    Fix image path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Ece-Books-Portal/',
   plugins: [react()],
-
-  base: "/Ece-Books-Portal/",
 })
