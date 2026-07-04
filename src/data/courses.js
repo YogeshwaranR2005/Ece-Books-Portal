0b7a60bc030ec015347b314e0a2d30746143e9b2
Yogeshwaran <yogeshrami2005@gmail.com>
2026-07-04 19:05:49 +0530

    Added new courses and books

--- src/data/courses.js
+++ src/data/courses.js
@@ -54,6 +54,84 @@ const courses = [
 
     ]
 },
+
+ {
+    id: "electric-and-magnetic-circuits",
+
+    name: "Electric and Magnetic Circuits",
+
+    books: [
+
+        {
+            id: 1,
+            title: "Engineering Circuit Analysis, 10th Edition (HAYT)",
+            file: "books/Engineering Circuit Analysis, 10th Edition (HAYT).pdf",
+        },
+
+        {
+            id: 2,
+            title: "Circuits and Network Analysis and Synthesis (Anant Sudhakar, Shyammohan S. Palli)",
+            file: "books/Circuits and Network Analysis and Synthesis (Anant Sudhakar, Shyammohan S. Palli).pdf",
+        }
+
+    ]
+},
+
+{
+    id: "electronic-devices",
+
+    name: "Electronic Devices",
+
+    books: [
+
+        {
+            id: 1,
+            title: "Electronic Devices and Circuit Theory Pearson New International 11th Edition (Boylestad, Robert L., Nashelsky, Louis.)",
+            file: "Electronic Devices and Circuit Theory Pearson New International 11th Edition (Boylestad, Robert L., Nashelsky, Louis.).pdf",
+        },
+
+        {
+            id: 2,
+            title: "Electronic Principles (Albert P. Malvino, David J. Bates etc.)",
+            file: "books/Electronic Principles (Albert P. Malvino, David J. Bates etc.).pdf",
+        }
+
+    ]
+},
+
+{
+    id: "field-theory-and-transmission-lines",
+
+    name: "Field Theory and Transmission Lines",
+
+    books: [
+
+        {
+            id: 1,
+            title: "Fundamentals of Applied Electromagnetics (8th Edition) (Fawwaz T. Ulaby, Umberto Ravaioli)",
+            file: "Fundamentals of Applied Electromagnetics (8th Edition) (Fawwaz T. Ulaby, Umberto Ravaioli).pdf",
+        },
+
+        {
+            id: 2,
+            title: "Electromagnetic Field Theory and Transmission Lines (G. S. N. Raju)",
+            file: "books/Electromagnetic Field Theory and Transmission Lines (G. S. N. Raju).pdf",
+        },
+
+        {
+            id: 3,
+            title: "Microwave Engineering (David M. Pozar)",
+            file: "books/Microwave Engineering (David M. Pozar).pdf",
+        },
+
+          {
+            id: 4,
+            title: "Elements of electromagnetics (Sadiku, Matthew N. O)",
+            file: "books/Elements of electromagnetics (Sadiku, Matthew N. O).pdf",
+        },
+
+    ]
+},
 ];
 
 export default courses;
